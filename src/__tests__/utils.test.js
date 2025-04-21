425cb2f359a453ae0cbf8b35823018d0a135b6e0
Faith <kashfaith77@gmail.com>
2025-04-21 19:04:33 +0300

    Write unit tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,41 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("check wheather a word is palindrome )", () => {
+    const word = "madam";
+
+    const palindrome = isPalindrome(word);
+
+    expect(palindrome).toBe(true);
+  });
+  it("check wheather a word is not palindrome )", () => {
+    const word = "cat";
+
+    const palindrome = isPalindrome(word);
+
+    expect(palindrome).toBe(false);
+  });
+  it("check wheather a word has upper and lowercase letters )", () => {
+    const word = "mAdam";
+
+    const palindrome = isPalindrome(word);
+
+    expect(palindrome).toBe(true);
+  });
+  it("check wheather a word is an empty string", () => {
+    const word = "";
+
+    const palindrome = isPalindrome(word);
+
+    expect(palindrome).toBe(false);
+  });
+
+  it("should check for non-numeric characters", () => {
+    const word = "12wepo";
+
+    expect(() => isPalindrome(word)).toThrow(
+      new Error("Numeric characters detected")
+    );
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
 // Your code here
+export function isPalindrome(word) {
+  const wordLowerCase = word.toLowerCase();
+  const reverse = wordLowerCase.split("").reverse().join("");
+  console.log(reverse, word);
+
+  const test = /^[A-Za-z]+$/.test(word);
+
+  if(word !== '' && !test) {
+    throw new Error('Numeric characters detected')
+  }
+
+  if(word===""){
+    return false
+  }
+  if (word.toLowerCase() === reverse) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
